763f9b1f9bcdb2844d22820d2698125f17cf2605
niyaz <mohemmedniyaz11@gmail.com>
2026-02-23 15:13:58 +0530

    committed

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+require("dotenv").config();
+const express = require("express");
+const app = express()
+const {connectDB}=require("./db")
 
+connectDB()
+
+const authRoutes = require("./routes/authRoutes");
+const preferenceRoutes = require("./routes/prefernceRoute");
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/preferences", preferenceRoutes);
+
+
+// app.use(express.urlencoded({ extended: true }));
+
+const port = process.env.PORT || 3000
 
 app.listen(port, (err) => {
     if (err) {
@@ -11,7 +23,5 @@ app.listen(port, (err) => {
     }
     console.log(`Server is listening on ${port}`);
 });
-
-
 
 module.exports = app;
---
+++ controllers/prefenceController.js
@@ -0,0 +1,68 @@
+const Preference = require("../models/Prefernce");
+
+// POST /preferences (Create)
+exports.createPreference = async (req, res) => {
+  try {
+    const { categories, languages } = req.body;
+
+    const existing = await Preference.findOne({ user: req.user.id });
+    if (existing) {
+      return res.status(400).json({ message: "Preferences already exist" });
+    }
+
+    const preference = await Preference.create({
+      user: req.user.id,
+      categories,
+      languages
+    });
+
+    res.status(201).json({
+      message: "Preferences created",
+      preference
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+// GET /preferences
+exports.getPreference = async (req, res) => {
+  try {
+    const preference = await Preference.findOne({ user: req.user.id });
+
+    if (!preference) {
+      return res.status(404).json({ message: "Preferences not found" });
+    }
+
+    res.status(200).json(preference);
+
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+// PUT /preferences (Update)
+exports.updatePreference = async (req, res) => {
+  try {
+    const { categories, languages } = req.body;
+
+    const preference = await Preference.findOneAndUpdate(
+      { user: req.user.id },
+      { categories, languages },
+      { new: true }
+    );
+
+    if (!preference) {
+      return res.status(404).json({ message: "Preferences not found" });
+    }
+
+    res.status(200).json({
+      message: "Preferences updated",
+      preference
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
